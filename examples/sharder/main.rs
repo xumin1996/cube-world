9491e0f58cf66e74c60d17e83a882cd259f9615e
x <xh@x.com>
2024-11-27 21:23:25 +0800

    简单cube例子

---
+++ examples/sharder/main.rs
@@ -0,0 +1,56 @@
+use avian3d::{parry::shape, prelude::*};
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy::prelude::*;
+use bevy_tnua::prelude::*;
+use bevy_tnua_avian3d::TnuaAvian3dPlugin;
+use smooth_bevy_cameras::LookTransformPlugin;
+use bevy::input::mouse::MouseMotion;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, startup)
+        .add_systems(Update, handle_mouse_motion)
+        .run();
+}
+
+pub fn startup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // 方块
+    commands.spawn((PbrBundle {
+        mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+        material: materials.add(Color::WHITE),
+        ..default()
+    },));
+
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
+pub fn handle_mouse_motion(
+    mut mouse_motion_events: EventReader<MouseMotion>,
+    mut camera_transform: Query<&mut Transform, With<Camera>>,
+) {
+    let displacement = mouse_motion_events
+        .read()
+        .fold(0., |acc, mouse_motion| acc + mouse_motion.delta.x);
+
+    // 旋转
+    camera_transform.single_mut().rotate_around(Vec3::ZERO, Quat::from_rotation_y(-displacement / 500.));
+}
