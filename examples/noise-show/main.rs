fc2621a8bc644477a90df7fc7ee4db6e90152ca0
xh <xh@t.com>
2024-12-08 00:35:46 +0800

    map例程

--- examples/noise-show/main.rs
+++ examples/noise-show/main.rs
@@ -1,17 +1,13 @@
 use bevy::input::mouse::MouseMotion;
+use bevy::prelude::*;
 use bevy::render::{
     mesh::Indices, render_asset::RenderAssetUsages, render_resource::PrimitiveTopology,
 };
-use bevy::{
-    prelude::*,
-    reflect::TypePath,
-    render::render_resource::{AsBindGroup, ShaderRef},
-};
-use noise::{NoiseFn, Perlin};
+use simdnoise::*;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins))
+        .add_plugins(DefaultPlugins)
         .add_systems(Startup, startup)
         .add_systems(Update, handle_mouse_motion)
         .run();
@@ -23,23 +19,20 @@ pub fn startup(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     // 平面
-    let plain_size = 128u32;
-    let mut plain_height: Vec<Vec<f32>> = Vec::new();
-    for plain_x in 0..=plain_size {
-        let mut z_height: Vec<f32> = Vec::new();
-        for plain_z in 0..=plain_size {
-            let block_x = plain_x as f64 / plain_size as f64;
-            let block_z = plain_z as f64 / plain_size as f64;
-            let height = get_height(block_x, block_z);
-            z_height.push(height);
-        }
-        plain_height.push(z_height);
-    }
+    let plain_size = 64u32;
+    let heights =
+        NoiseBuilder::fbm_2d((plain_size + 1) as usize, (plain_size + 1) as usize).generate_scaled(0.0, 1.0);
+    println!("size {:?}", heights.len());
+    let plain_height: Vec<Vec<f32>> = heights
+        .chunks((plain_size + 1) as usize)
+        .map(|chunk| chunk.to_vec())
+        .collect();
+
     commands.spawn(PbrBundle {
         mesh: meshes.add(create_plain_mesh(
             plain_size,
             &plain_height,
-            Transform::from_xyz(0f32, 0f32, 0f32),
+            Transform::from_xyz(0.5f32, 0f32, 0.5f32),
         )),
         material: materials.add(Color::WHITE),
         ..default()
@@ -76,21 +69,6 @@ pub fn handle_mouse_motion(
         .rotate_around(Vec3::ZERO, Quat::from_rotation_y(-displacement / 700.));
 }
 
-#[derive(Asset, TypePath, AsBindGroup, Debug, Clone)]
-struct CustomMaterial {}
-
-impl Material for CustomMaterial {
-    fn fragment_shader() -> ShaderRef {
-        "shaders/animate_shader.wgsl".into()
-    }
-}
-
-fn get_height(x: f64, z: f64) -> f32 {
-    let perlin = Perlin::new(1);
-    let height = perlin.get([x, z]);
-    return height as f32 * 500.0f32;
-}
-
 // 创建平面网格 (plain_size+1)*(plain_size+1) x,y
 fn create_plain_mesh(plain_size: u32, height_mesh: &Vec<Vec<f32>>, transform: Transform) -> Mesh {
     let mut attribute_position: Vec<[f32; 3]> = Vec::new();
@@ -101,7 +79,7 @@ fn create_plain_mesh(plain_size: u32, height_mesh: &Vec<Vec<f32>>, transform: Tr
     for (x_index, z_list) in height_mesh.iter().enumerate() {
         for (z_index, y_height) in z_list.iter().enumerate() {
             // 顶点
-            let cube_size = 1f32;
+            let cube_size = 1f32 / plain_size as f32;
             let x = cube_size * x_index as f32 + transform.translation.x;
             let y = *y_height;
             let z = cube_size * z_index as f32 + transform.translation.z;
