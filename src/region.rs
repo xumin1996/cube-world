ce1453a3ceaedb0a04ee613af37209c92197ce6d
XuHang <hang.xu@lingshu.net>
2025-02-08 00:04:37 +0800

    region修改为使用默认cuboid

--- src/region.rs
+++ src/region.rs
@@ -1,16 +1,13 @@
 use std::time::Instant;
 
+use crate::player::Player;
 use crate::util::Triangle;
-use crate::{customMaterial::CustomMaterial, player::Player};
 use bevy::gltf::{Gltf, GltfMesh, GltfNode};
 use bevy::prelude::*;
-use bevy::render::mesh;
 use bevy::render::{
     mesh::Indices, render_asset::RenderAssetUsages, render_resource::PrimitiveTopology,
 };
-use bevy::time::Stopwatch;
 use bevy_rapier3d::prelude::*;
-use bevy_rapier3d::rapier::prelude::RigidBodyActivation;
 use simdnoise::*;
 
 #[derive(Component, Debug)]
@@ -118,14 +115,17 @@ pub fn region_update(
         .and_then(|floor_mesh_handle| gltf_mesh_asset.get(floor_mesh_handle))
     {
         // 加载 PBR 贴图
-        let base_color_texture: Handle<Image> = asset_server.load("textures/cobblestone.png");
-        let normal_texture: Handle<Image> = asset_server.load("textures/cobblestone_n.png");
-        let metallic_roughness_texture: Handle<Image> = asset_server.load("textures/cobblestone_s.png");
+        let base_color_texture: Handle<Image> = asset_server.load("textures/barrel_side.png");
+        let normal_texture: Handle<Image> = asset_server.load("textures/barrel_side_n.png");
+        let metallic_roughness_texture: Handle<Image> =
+            asset_server.load("textures/barrel_side_mr.png");
 
         // 创建 PBR 材质
         let material = materials.add(StandardMaterial {
             base_color_texture: Some(base_color_texture),
             normal_map_texture: Some(normal_texture),
+            metallic: 1.0,
+            perceptual_roughness: 1.0,
             metallic_roughness_texture: Some(metallic_roughness_texture),
             ..default()
         });
@@ -257,7 +257,9 @@ fn region_by_block(region_x: i32, region_z: i32) -> Mesh {
 
     let start = Instant::now();
     let mut collider_cube_mesh = create_cube_mesh(&plain_height);
-    collider_cube_mesh.generate_tangents().expect("generate_tangents fail");
+    collider_cube_mesh
+        .generate_tangents()
+        .expect("generate_tangents fail");
     println!(
         "create mesh time: {}",
         (Instant::now() - start).as_secs_f32()
@@ -341,7 +343,7 @@ fn create_plain_mesh(height_mesh: &Vec<Vec<f32>>, transform: Transform) -> Mesh 
 
 fn create_cube_mesh(height_mesh: &Vec<Vec<f32>>) -> Mesh {
     let plain_size = 16usize;
-    let mut cube_transform = Vec::<Transform>::new();
+    let mut cube_transform: Vec<Transform> = Vec::<Transform>::new();
 
     for (x_index, z_list) in height_mesh.iter().take(plain_size).enumerate() {
         for (z_index, y_height) in z_list.iter().take(plain_size).enumerate() {
@@ -354,144 +356,12 @@ fn create_cube_mesh(height_mesh: &Vec<Vec<f32>>) -> Mesh {
         }
     }
 
-    return create_cubes(&cube_transform);
-}
+    let mut cube_mesh: Triangle = Triangle::from_mesh(&Cuboid::new(1.0, 1.0, 1.0).mesh().build());
+    cube_mesh.uv.iter_mut().for_each(|item|{
+        item.x = item.x / 1.0;
+        item.y = item.y / 1.0; 
+    });
+    let plain_mesh: Triangle = cube_mesh * cube_transform;
 
-// 构造Mesh
-fn create_cubes(cube_positions: &Vec<Transform>) -> Mesh {
-    let mut attribute_position: Vec<[f32; 3]> = Vec::new();
-    let mut attribute_uv_0: Vec<[f32; 2]> = Vec::new();
-    let mut attribute_normal: Vec<[f32; 3]> = Vec::new();
-    let mut indices: Vec<u32> = Vec::new();
-
-    for cube_position in cube_positions.iter() {
-        let pre_attribute_position_num = attribute_position.len();
-        let mut item_position = vec![
-            // top (facing towards +y)
-            [-0.5, 0.5, -0.5], // vertex with index 0
-            [0.5, 0.5, -0.5],  // vertex with index 1
-            [0.5, 0.5, 0.5],   // etc. until 23
-            [-0.5, 0.5, 0.5],
-            // bottom   (-y)
-            [-0.5, -0.5, -0.5],
-            [0.5, -0.5, -0.5],
-            [0.5, -0.5, 0.5],
-            [-0.5, -0.5, 0.5],
-            // right    (+x)
-            [0.5, -0.5, -0.5],
-            [0.5, -0.5, 0.5],
-            [0.5, 0.5, 0.5], // This vertex is at the same position as vertex with index 2, but they'll have different UV and normal
-            [0.5, 0.5, -0.5],
-            // left     (-x)
-            [-0.5, -0.5, -0.5],
-            [-0.5, -0.5, 0.5],
-            [-0.5, 0.5, 0.5],
-            [-0.5, 0.5, -0.5],
-            // back     (+z)
-            [-0.5, -0.5, 0.5],
-            [-0.5, 0.5, 0.5],
-            [0.5, 0.5, 0.5],
-            [0.5, -0.5, 0.5],
-            // forward  (-z)
-            [-0.5, -0.5, -0.5],
-            [-0.5, 0.5, -0.5],
-            [0.5, 0.5, -0.5],
-            [0.5, -0.5, -0.5],
-        ];
-        for mut position_item in item_position.iter_mut() {
-            position_item[0] += cube_position.translation.x;
-            position_item[1] += cube_position.translation.y;
-            position_item[2] += cube_position.translation.z;
-        }
-
-        attribute_position.extend(item_position);
-        attribute_uv_0.extend(vec![
-            // Assigning the UV coords for the top side.
-            [0.0, 1.0],
-            [0.0, 0.0],
-            [1.0, 0.0],
-            [1.0, 1.0],
-            // Assigning the UV coords for the bottom side.
-            [0.0, 1.0],
-            [0.0, 0.0],
-            [1.0, 0.0],
-            [1.0, 1.0],
-            // Assigning the UV coords for the right side.
-            [1.0, 1.0],
-            [0.0, 1.0],
-            [0.0, 0.0],
-            [1.0, 0.0],
-            // Assigning the UV coords for the left side.
-            [1.0, 1.0],
-            [0.0, 1.0],
-            [0.0, 0.0],
-            [1.0, 0.0],
-            // Assigning the UV coords for the back side.
-            [0.0, 1.0],
-            [0.0, 0.0],
-            [1.0, 0.0],
-            [1.0, 1.0],
-            // Assigning the UV coords for the forward side.
-            [0.0, 1.0],
-            [0.0, 0.0],
-            [1.0, 0.0],
-            [1.0, 1.0],
-        ]);
-        attribute_normal.extend(vec![
-            // Normals for the top side (towards +y)
-            [0.0, 1.0, 0.0],
-            [0.0, 1.0, 0.0],
-            [0.0, 1.0, 0.0],
-            [0.0, 1.0, 0.0],
-            // Normals for the bottom side (towards -y)
-            [0.0, -1.0, 0.0],
-            [0.0, -1.0, 0.0],
-            [0.0, -1.0, 0.0],
-            [0.0, -1.0, 0.0],
-            // Normals for the right side (towards +x)
-            [1.0, 0.0, 0.0],
-            [1.0, 0.0, 0.0],
-            [1.0, 0.0, 0.0],
-            [1.0, 0.0, 0.0],
-            // Normals for the left side (towards -x)
-            [-1.0, 0.0, 0.0],
-            [-1.0, 0.0, 0.0],
-            [-1.0, 0.0, 0.0],
-            [-1.0, 0.0, 0.0],
-            // Normals for the back side (towards +z)
-            [0.0, 0.0, 1.0],
-            [0.0, 0.0, 1.0],
-            [0.0, 0.0, 1.0],
-            [0.0, 0.0, 1.0],
-            // Normals for the forward side (towards -z)
-            [0.0, 0.0, -1.0],
-            [0.0, 0.0, -1.0],
-            [0.0, 0.0, -1.0],
-            [0.0, 0.0, -1.0],
-        ]);
-
-        let indices_temp: Vec<u32> = vec![
-            0, 3, 1, 1, 3, 2, // triangles making up the top (+y) facing side.
-            4, 5, 7, 5, 6, 7, // bottom (-y)
-            8, 11, 9, 9, 11, 10, // right (+x)
-            12, 13, 15, 13, 14, 15, // left (-x)
-            16, 19, 17, 17, 19, 18, // back (+z)
-            20, 21, 23, 21, 22, 23, // forward (-z)
-        ];
-        let indices_maped: Vec<u32> = indices_temp
-            .iter()
-            .map(|item| item + pre_attribute_position_num as u32)
-            .collect();
-        indices.extend(indices_maped);
-    }
-
-    // Keep the mesh data accessible in future frames to be able to mutate it in toggle_texture.
-    Mesh::new(
-        PrimitiveTopology::TriangleList,
-        RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
-    )
-    .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, attribute_position)
-    .with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, attribute_uv_0)
-    .with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, attribute_normal)
-    .with_inserted_indices(Indices::U32(indices))
+    plain_mesh.build()
 }
